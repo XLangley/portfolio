2451606f05a2ae5168de15e966d3d3be0b28ef56
xLangley <alfred.egs@gmail.com>
2026-09-14 20:27:44 -0300

    feat: add @vercel/speed-insights for performance monitoring

--- src/main.tsx
+++ src/main.tsx
@@ -6,9 +6,11 @@ import "@fontsource/jetbrains-mono/400.css"
 import "@fontsource/jetbrains-mono/500.css"
 import "./index.css"
 import App from './App.tsx'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <SpeedInsights />
   </StrictMode>,
 )
